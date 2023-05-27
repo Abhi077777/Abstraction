7a2b6b57443ae98a38d05f15c8c595e86e68ee68
Abhi077777 <120783717+Abhi077777@users.noreply.github.com>
2023-05-27 18:28:57 +0530

    Add project files.

---
+++ Abstractclass&Method/Abstractcalss.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractclass_Method
+{
+
+    class Program
+    { 
+         static void Main()
+        {
+            Employee e = new Employee(333,"Abhishek",50000);
+            e.showEmployeeDetails();
+            Console.ReadLine();
+        }
+    }
+    class Employee
+    {
+        public int EmpID;
+        public string EmpName;
+
+        public double Grosspay;
+        double Taxdeduction = 0.1;
+        double netSalary;
+
+
+        public Employee(int Eid, string EName, double Egrosspay) 
+        { 
+
+            this.EmpID = Eid;
+            this.EmpName = EName;
+            this.Grosspay = Egrosspay;
+        }
+
+        void CalculateSalary()
+        {
+            if (Grosspay >= 30000)
+            {
+                netSalary = Grosspay - (Taxdeduction * Grosspay);
+                Console.WriteLine("your salary is: {0}",netSalary);
+            }
+            else
+            {
+                Console.WriteLine("Your salary is: {0}",Grosspay);
+            }
+        }
+        public void showEmployeeDetails()
+        {
+            this.CalculateSalary();
+        }
+
+    }
+}
---
+++ Abstractclass&Method/Program.cs
@@ -0,0 +1,166 @@
+﻿//using System;
+
+//abstract class Name
+//{
+//    public void Display()
+//    {
+//        Console.WriteLine("Non Abstract class");
+//    }
+
+
+
+//}
+
+//class Abstrct : Name
+//{
+//    public static void Main()
+//    {
+
+//        Abstrct a1 = new Abstrct();
+//        a1.Display();
+//        Console.ReadLine();
+//    }
+//}
+    
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//using System;
+//namespace AbstractClass
+//{
+
+//    abstract class Animal
+//    {
+
+//        // abstract method
+//        public abstract void makeSound();
+//    }
+
+//    // inheriting from abstract class
+//    class Dog : Animal
+//    {
+
+//        // provide implementation of abstract method
+//        public override void makeSound()
+//        {
+
+//            Console.WriteLine("Bark Bark");
+
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // create an object of Dog class
+//            Dog obj = new Dog();
+//            obj.makeSound();
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//using System;
+//namespace AbstractClass
+//{
+//    abstract class Animal
+//    {
+
+//        protected string name;
+//        // abstract method
+//        public abstract string Name
+//        {
+//            get;
+//            set;
+//        }
+//    }
+
+//    // inheriting from abstract class
+//    class Dog : Animal
+//    {
+
+//        // provide implementation of abstract method
+//        public override string Name
+//        {
+//            get
+//            {
+//                return name;
+//            }
+//            set
+//            {
+//                name = value;
+//            }
+//        }
+
+//    }
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            // create an object of Dog class
+//            Dog obj = new Dog();
+//            obj.Name = "Tom";
+//            Console.WriteLine("Name: " + obj.Name);
+
+//            Console.ReadLine();
+//        }
+//    }
+//    }
+
+
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+//Nested Class ---we can define the class within an other class.
+
+
+//using System;
+
+
+// public class car
+//{
+//    public string Brand =  "Mahindra";
+
+
+//    public class Engine
+//    {
+//        public void displaycar()
+//        {
+//            car sportscar = new car();
+//           Console.WriteLine("Brand: " +  sportscar.Brand);
+//        }
+//    }
+
+//}
+
+//class Program
+//{
+//    public static void Main()
+//    {
+//        car.Engine MountinCar  = new car.Engine();
+//        MountinCar.displaycar();
+//        Console.ReadLine();
+//    }
+//}
+
+//----------------------------Static class-----------------------------------------------------------------------------------------------------------
+
+//using System;
+//public static class MyMath
+//{
+//    public static float PI = 3.14f;
+//    public static int cube(int n) { return n * n * n; }
+//}
+//class TestMyMath
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Value of PI is: " + MyMath.PI);
+//        Console.WriteLine("Cube of 3 is: " + MyMath.cube(3));
+//    }
+//}
+
